f8dcc12dc41c631c7a4cc8078c416f401c2e34fe
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-10-03 22:57:44 -0500

    angular2-material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+/*material design*/
+import { MdCardModule } from '@angular2-material/card';
+import { MdButtonModule } from '@angular2-material/button';
+import {MdIconModule} from '@angular2-material/icon';
 
+import {MdIconRegistry } from '@angular2-material/icon';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,MdCardModule,MdButtonModule,MdIconModule
   ],
-  providers: [],
+  providers: [MdIconRegistry],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
