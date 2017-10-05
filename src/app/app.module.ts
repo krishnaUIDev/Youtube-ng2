b69ed2ac3852f616fd46ba660618f45cbd043ae4
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-10-04 22:02:37 -0500

    http added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-/*material design*/
-import { MdCardModule } from '@angular2-material/card';
-import { MdButtonModule } from '@angular2-material/button';
-import {MdIconModule} from '@angular2-material/icon';
-import { MdCheckboxModule} from '@angular/material';
-import {MdIconRegistry } from '@angular2-material/icon';
+
+import {BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule} from "@angular/material";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,MdCardModule,MdButtonModule,MdIconModule,MdCheckboxModule
+    BrowserModule,BrowserAnimationsModule,HttpModule,
+    MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule
   ],
-  providers: [MdIconRegistry],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
